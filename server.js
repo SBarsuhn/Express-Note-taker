662462e7e0bed71940b2facde53ff4434600c067
SBars <samuelbarsuhn@gmail.com>
2023-02-28 14:19:40 -0600

    added code to server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoute')
+const apiRoutes = require('./routes/apiRoute')
+const PORT = process.env.port || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/', htmlRoutes)
+app.use('/api', apiRoutes)
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
