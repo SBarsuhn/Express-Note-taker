f5cbb54919159b50b298d23b9fd63e9c41869f69
SBars <samuelbarsuhn@gmail.com>
2023-03-02 18:19:57 -0600

    fixed the port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoute')
 const apiRoutes = require('./routes/apiRoute')
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
